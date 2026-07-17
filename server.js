5fad1e4a119a569fc0ebb1b0aa1dda2c89fc78c2
Wilson Villarreal <villarrealgomezwilson@gmail.com>
2026-07-17 16:07:32 -0600

    Fix: Ruta absoluta para el despliegue

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Servir los archivos de la carpeta public
-app.use(express.static('public'));
+// Servir archivos usando una ruta absoluta infalible
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Forzar la carga del index en la raíz
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en http://localhost:${PORT}`);
